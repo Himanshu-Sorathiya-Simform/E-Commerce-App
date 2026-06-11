96724691dc019097ed074189609bcdf064f61e9f
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-06-11 10:11:13 +0530

    add axios package for data fetching

--- src/context/categoriesContext.tsx
+++ src/context/categoriesContext.tsx
@@ -5,7 +5,7 @@ import {
 	useEffect,
 	useState,
 } from "react";
-import { fetchCategories } from "../api/categoryApi.ts";
+import { fetchCategories } from "../services/categoryApi.ts";
 
 interface CategoriesProviderProps {
 	children: ReactNode;
