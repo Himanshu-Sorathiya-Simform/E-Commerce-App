f010c0db874851948be5c0a0fbebbcc9759c58bd
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-06-18 15:36:38 +0530

    add shadcn deps for signin and signup components

--- src/components/footer/FooterNavLink.tsx
+++ src/components/footer/FooterNavLink.tsx
@@ -8,6 +8,10 @@ function FooterNavLink({ link }: FooterNavLinkProps) {
 	return (
 		<li className="footer-nav-item">
 			<a
+				style={{
+					width: "100%",
+					display: "inline-block",
+				}}
 				className="footer-nav-link link"
 				href={`/${link.href}`}
 			>
