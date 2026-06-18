fab528182853b749a0276dc456e85e8e345a0841
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-06-18 15:26:00 +0530

    initize router and routes object

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,22 @@
-import { ConfigProvider, Drawer, Flex } from "antd";
-import { useState } from "react";
-import CartInformation from "./components/cart/CartInformation.tsx";
-import Content from "./components/content/Content.tsx";
-import Header from "./components/header/Header.tsx";
-import Sidebar from "./components/sidebar/Sidebar.tsx";
-import CartProvider from "./context/cartContext.tsx";
-import CategoriesProvider from "./context/categoriesContext.tsx";
-import ProductsProvider from "./context/productsContext.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import AppLayout from "./layouts/AppLayout.tsx";
+import RootLayout from "./layouts/RootLayout.tsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		Component: RootLayout,
+		children: [
+			{
+				index: true,
+				Component: AppLayout,
+			},
+		],
+	},
+]);
 
 function App() {
-	const [collapsed, setCollapsed] = useState(false);
-	const [cartDrawerOpen, setCartDrawerOpen] = useState(false);
-
-	function openCartDrawer() {
-		setCartDrawerOpen(true);
-	}
-
-	return (
-		<CategoriesProvider>
-			<ProductsProvider>
-				<CartProvider>
-					<ConfigProvider
-						theme={{
-							token: {
-								colorPrimary: "#003d29",
-								fontFamily:
-									'"General Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
-							},
-							components: {
-								Layout: {
-									headerBg: "var(--color-secondary)",
-									footerBg: "var(--color-secondary)",
-									headerPadding: 0,
-									headerHeight: "auto",
-								},
-							},
-						}}
-					>
-						<Flex vertical>
-							<Flex
-								vertical
-								style={{
-									height: "100vh",
-									overflow: "hidden",
-								}}
-							>
-								<Header openCartDrawer={openCartDrawer} />
-
-								<Flex
-									style={{
-										height: "100%",
-										position: "relative",
-										overflow: "hidden",
-									}}
-								>
-									<Sidebar collapsed={collapsed} />
-
-									<Content setCollapsed={setCollapsed} />
-								</Flex>
-							</Flex>
-						</Flex>
-
-						<Drawer
-							title="Your Cart"
-							placement="right"
-							size={"30%"}
-							open={cartDrawerOpen}
-							onClose={() => setCartDrawerOpen(false)}
-							mask={{ blur: true }}
-						>
-							<CartInformation />
-						</Drawer>
-					</ConfigProvider>
-				</CartProvider>
-			</ProductsProvider>
-		</CategoriesProvider>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
