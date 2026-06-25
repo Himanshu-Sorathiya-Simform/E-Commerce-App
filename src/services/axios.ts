48309bb81926b661a1546d24cdba93986f9bcf4c
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-06-25 16:48:28 +0530

    create axios base quer wrapper

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,9 +1,31 @@
-import type { AxiosInstance } from "axios";
-import axios from "axios";
+import axios, {
+	type AxiosInstance,
+	type AxiosRequestConfig,
+	AxiosError,
+} from "axios";
 
 const api: AxiosInstance = axios.create({
 	baseURL: "https://dummyjson.com",
 	timeout: 5000,
 });
 
-export { api };
+function axiosBaseQuery() {
+	return async function (args: AxiosRequestConfig) {
+		try {
+			const result = await api(args);
+
+			return { data: result.data };
+		} catch (axiosError) {
+			const err = axiosError as AxiosError;
+
+			return {
+				error: {
+					status: err.response?.status,
+					data: err.response?.data || err.message,
+				},
+			};
+		}
+	};
+}
+
+export { api, axiosBaseQuery };
